d7eec1e646cbf86d530d5daaf726884aca771b0b
gophic <a11574@126.com>
2018-11-13 17:13:47 +0800

    update some mistakes.

--- src/main/java/com/csg/exchange/service/httpSender/HttpRequestSenderImpl.java
+++ src/main/java/com/csg/exchange/service/httpSender/HttpRequestSenderImpl.java
@@ -1,29 +1,17 @@
 package com.csg.exchange.service.httpSender;
 
-import com.csg.exchange.service.httpSender.HttpRequestSender;
-import com.csg.exchange.service.webserviceClient.AlertInfo;
-import org.apache.http.HttpClientConnection;
-import org.apache.http.client.HttpClient;
 import org.apache.http.entity.StringEntity;
 import org.jetbrains.annotations.Contract;
 import net.sf.json.JSONObject;
-import com.csg.exchange.service.webserviceClient.AlertInfo;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import org.apache.http.Consts;
+
 import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
 import org.apache.http.ParseException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 
@@ -43,6 +31,7 @@ public class HttpRequestSenderImpl implements HttpRequestSender{
             StringEntity s = new StringEntity(info.toString());
             s.setContentEncoding("UTF-8");
             s.setContentType("application/json");//发送json数据需要设置contentType
+            System.out.print(s.getContent());
             httppost.setEntity(s);
             response = httpclient.execute(httppost);
         } catch (IOException e) {
